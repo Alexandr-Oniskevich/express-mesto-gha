1e2d8cfe7563e117affc5143965a54229ce7756d
Alexandr Oniskevich <alexandroniskevich93@gmail.com>
2023-03-05 23:22:40 +0300

    second iteration

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
+const { ERROR_CODE } = require('./utils/constants');
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+app.use(helmet());
+app.disable('x-powered-by');
+
+app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
     _id: '6402678bbf70b2361ac983cb',
   };
-
   next();
 });
 
@@ -28,7 +37,7 @@ app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
+  res.status(ERROR_CODE).send({ message: 'Запрашиваемая страница не найдена' });
 });
 
 app.listen(PORT, () => {
--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,38 @@
 const User = require('../models/user');
 
+const {
+  ERROR_CODE, ERROR_NO_USER, ERROR_SERVER, SUCCESS,
+} = require('../utils/constants');
+
 const getUsers = (req, res) => User.find({})
-  .then((users) => res.status(200).send({ data: users }))
-  .catch((error) => res.status(500).send(`Ошибка сервера: ${error}`));
+  .then((users) => res.status(SUCCESS).send({ data: users }))
+  .catch((error) => res.status(ERROR_SERVER).send(`Ошибка сервера: ${error}`));
 
 const getUsersId = (req, res) => User.findById(req.params.userId)
-  .then((user) => res.status(200).send(user))
+  .then((user) => {
+    if (user) {
+      res.send({ data: user });
+    } else {
+      res.status(ERROR_NO_USER).send({ message: 'Пользователь по указанному _id не найден' });
+    }
+  })
   .catch((error) => {
     if (error.name === 'CastError') {
-      res.status(400).send({ message: 'Нет пользователя с таким ID' });
+      res.status(ERROR_CODE).send({ message: 'Некорректный _id пользователя' });
     } else {
-      res.status(500).send(`Ошибка сервера: ${error}`);
+      res.status(ERROR_SERVER).send(`Ошибка сервера: ${error}`);
     }
   });
 
 const createUsers = (req, res) => {
   const { name, about, avatar } = req.body;
   return User.create({ name, about, avatar })
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(SUCCESS).send({ data: user }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
       } else {
-        res.status(500).send(`Ошибка сервера: ${error}`);
+        res.status(ERROR_SERVER).send(`Ошибка сервера: ${error}`);
       }
     });
 };
@@ -36,9 +46,9 @@ const changeUserInfo = (req, res) => {
     .then((userInfo) => res.send({ data: userInfo }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
       } else {
-        res.status(500).send(`Ошибка сервера: ${error}`);
+        res.status(ERROR_SERVER).send(`Ошибка сервера: ${error}`);
       }
     });
 };
@@ -52,9 +62,9 @@ const changeAvatar = (req, res) => {
     .then((userAvatar) => res.send({ data: userAvatar }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
       } else {
-        res.status(500).send(`Ошибка сервера: ${error}`);
+        res.status(ERROR_SERVER).send(`Ошибка сервера: ${error}`);
       }
     });
 };
